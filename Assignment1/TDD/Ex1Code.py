df72b3e8736f0241c3cdc1da2ad9e49a14611ccb
SJEduard <s.j.eduard@student.rug.nl>
2023-12-14 21:04:45 +0100

    Red 2: Cannot take user input, still at initialized

--- Assignment1/TDD/Ex1Code.py
+++ Assignment1/TDD/Ex1Code.py
@@ -6,7 +6,11 @@ class CodeMaker:
     def __init__(self):
         self._colors = np.array([1,2,3,4,5,6])
         self._code = np.array([0,0,0,0]) # Initializing as empty strings
+        self._current_guess = np.array([0,0,0,0])
 
     def BuildCode(self):
         for i in range(4):
             self._code[i] = np.random.choice(self._colors)
+
+    def TakeUserInput(self):
+        return self._current_guess
--- Assignment1/TDD/Ex1Testing.py
+++ Assignment1/TDD/Ex1Testing.py
@@ -1,6 +1,5 @@
 import numpy as np
 import unittest
-import sys
 import Ex1Code
 
 
@@ -8,9 +7,12 @@ class Tests(unittest.TestCase):
     def FillingUpTheCode(self):
         testcode = Ex1Code.CodeMaker()
         testcode.BuildCode()
-        self.assertNotEqual(0,testcode._code[0])
+        self.assertNotEqual(0,testcode._code[0])  # By extension of this one working, so do the others.
 
-tester = Tests()
-tester.FillingUpTheCode()
+    def TakeUserInput(self):
+        testinput = Ex1Code.CodeMaker().TakeUserInput()# From words to numbers 1 through 6. Input will be "white white white white"
+        self.assertEqual(1, testinput[0])
+
+tester = Tests().TakeUserInput()
 
 print(tester)
